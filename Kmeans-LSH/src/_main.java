a9cc73e34c8d492c0beb36d96fc8c0d03e9fb0cb
Reshad Dernjani <a1409161@univie.ac.at>
2017-06-01 12:14:56 +0200

    LocalPath method

--- Kmeans-LSH/src/_main.java
+++ Kmeans-LSH/src/_main.java
@@ -1,8 +1,26 @@
+import java.io.*;
+import java.net.URISyntaxException;
+
 /**
  * Created by Reshad_Dee on 01.06.2017.
  */
 public class _main {
+
+    public static String getLocalPath(String filename){
+        File root = null;
+        try {
+            root = new File(Thread.currentThread().getContextClassLoader().getResource("").toURI());
+        } catch (URISyntaxException e1) {
+            e1.printStackTrace();
+        }
+        File resource = new File(root, filename);
+        String filePath = resource.toString();
+        return filePath;
+    }
+
+
     public static void main(String[] args){
-        System.out.println("Kmeans with LSH");
+        System.out.println("#####" + getLocalPath("test"));
+
     }
 }
