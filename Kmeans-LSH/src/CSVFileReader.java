7775f6117fc476e39bbfdbc5ace258931d9b4aba
Reshad Dernjani <a1409161@univie.ac.at>
2017-06-01 14:14:28 +0200

    CSV importer written

---
+++ Kmeans-LSH/src/CSVFileReader.java
@@ -0,0 +1,80 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import com.opencsv.CSVReader;
+
+/**
+ * Class is responsible for the Import of the Data source.
+ * @see <a href="http://opencsv.sourceforge.net/">http://opencsv.sourceforge.net/</a>
+ * @author <a href="mailto:a1409161@unet.univie.ac.at">Reshad Dernjnai</a>
+ */
+public class CSVFileReader {
+	
+	private char seperator;
+	private String pathOfCSVFile = null;
+	private int beginWithLine;
+	private ArrayList<Double[]> points = new ArrayList<Double[]> ();
+
+
+	public CSVFileReader(String csvPath, char seperator, int beginWithLine) throws IOException {
+		this.seperator = seperator;
+		this.beginWithLine = beginWithLine;
+		pathOfCSVFile = csvPath;
+		doImport();
+	}
+
+	public void doImport() throws IOException {
+		Double[] point = new Double[10];
+		String[] nextLine;
+		int counter = 0;
+		int lineNumber = 0;
+		FileReader fileReader = new FileReader(pathOfCSVFile);
+		CSVReader csvReader = null;
+		csvReader = new CSVReader(fileReader, seperator, '"', beginWithLine);
+		while ((nextLine = csvReader.readNext()) != null) {
+			if (nextLine != null) {
+				for (int i = 0; i < 10; i++) {
+					if(nextLine[i] != null)
+						point[i] = Double.parseDouble(nextLine[i]);
+					else
+						point[i] = 0.0;
+				}
+				points.add(point);
+				lineNumber++;
+				counter++;
+			}
+		}
+	}
+
+	public ArrayList<Double[]> getPoints() {
+		return points;
+	}
+
+	public void setPoints(ArrayList<Double[]> points) {
+		this.points = points;
+	}
+
+	public char getSeperator() {
+		return seperator;
+	}
+
+	public void setSeperator(char seperator) {
+		this.seperator = seperator;
+	}
+
+	public String getPathOfCSVFile() {
+		return pathOfCSVFile;
+	}
+
+	public void setPathOfCSVFile(String pathOfCSVFile) {
+		this.pathOfCSVFile = pathOfCSVFile;
+	}
+
+	public int getBeginWithLine() {
+		return beginWithLine;
+	}
+
+	public void setBeginWithLine(int beginWithLine) {
+		this.beginWithLine = beginWithLine;
+	}
+}
---
+++ Kmeans-LSH/src/Vis.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Reshad_Dee on 01.06.2017.
+ */
+public class Vis {
+}
