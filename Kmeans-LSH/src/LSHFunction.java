69841af72f67d5b01e0cd75a37db3eb28b593537
chain <chain@rpgfiction.net>
2017-06-06 21:02:48 +0200

    add LSHFunction base class

---
+++ Kmeans-LSH/src/LSHFunction.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Richard Homonnai on 03.06.2017.
+ */
+public class LSHFunction {
+	private double[] a;
+	private double b;
+
+	public void LSHFunction(double[] a, double b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public void LSHFunction() {
+		/* empty constructor */
+	}
+
+    public double[] getA() {
+        return this.a;
+    }
+    
+    public double getB() {
+        return this.b;
+    }
+    
+    public void setA(double[] a) {
+        this.a = a;
+    }
+    
+    public void setB(double b) {
+        this.b = b;
+    }
+}
