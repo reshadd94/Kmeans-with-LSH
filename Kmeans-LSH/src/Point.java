f3a6b09a9eb4edc06ee17975d5f6cdf0bbab3f04
Reshad Dernjani <a1409161@univie.ac.at>
2017-06-07 23:00:18 +0200

    Doc on kmeans and point class

--- Kmeans-LSH/src/Point.java
+++ Kmeans-LSH/src/Point.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 /**
+ * The class Point.java handles the data and is used as input for Kmeans.java!
+ *
  * Modified by Reshad_Dee on 06.06.2017.
  * <a href="mailto:a1409161@unet.univie.ac.at">Reshad Dernjnai</a>
  * Based on Point.java by Richard Homonnai
@@ -9,15 +11,25 @@ public class Point {
     double[] data;
     int c;
 
+    /**
+     * The constructor gets the array of data/dimensions of point and c as centroid,
+     * which should be init by zero.
+     */
     public Point(double[] data, int c) {
         this.c = c;
         this.data = data;
     }
 
+    /**
+     * Returns the data point array.
+     */
     public double[] getData() {
         return data;
     }
 
+    /**
+     * Returns a dimension of the data point.
+     */
 	public double getDataPoint(int i) {
 		return data[i];
 	}
